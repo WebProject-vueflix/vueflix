8e3ee7e348ccaddf399556809f67e7277ba87ec8
aeri <dofl8787@naver.com>
2022-05-19 11:38:02 +0900

    dump data 01

--- final-pjt-back/movies/models.py
+++ final-pjt-back/movies/models.py
@@ -7,59 +7,71 @@ class Actor(models.Model):
     name = models.CharField(max_length=100)
     profile_path = models.CharField(max_length=200, null=True)
     character = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
 
 
 class Director(models.Model):
     name = models.CharField(max_length=200)
     profile_path = models.CharField(max_length=200, null=True)
+    def __str__(self):
+        return self.name
 
-class Video(models.Model):
-    key = models.CharField(max_length=100)
 
 class Genre(models.Model):
     name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
+
 
 class PopularMovie(models.Model):
     like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_popular_movies')
     genres = models.ManyToManyField(Genre, related_name='popular_movies')
-    actor = models.ManyToManyField(Actor, related_name='popular_movies')
-    director = models.ManyToManyField(Director, related_name='popular_movies')
+    actors = models.ManyToManyField(Actor, related_name='popular_actors')
+    director = models.ManyToManyField(Director, related_name='popular_director')
     adult = models.BooleanField()
-    movie_id = models.IntegerField(null=True)
     title = models.CharField(max_length=100)
     release_date = models.DateField()
     popularity = models.FloatField()
     vote_average = models.FloatField()
     overview = models.TextField()
+    youtube_key = models.CharField(max_length=200)
     backdrop_path = models.CharField(max_length=200, null=True)
     poster_path = models.CharField(max_length=200, null=True)
-
+    def __str__(self):
+        return self.name
 class NowPlayingMovie(models.Model):
     like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_nowplaying_movies')
-    genres = models.ManyToManyField(Genre, related_name='nowplaying_movies')
+    genres = models.ManyToManyField(Genre, related_name='nowplaying_movies')    
+    actors = models.ManyToManyField(Actor, related_name='nowplaying_actors')
+    director = models.ManyToManyField(Director, related_name='nowplaying_director')
     adult = models.BooleanField()
-    movie_id = models.IntegerField(null=True)
     title = models.CharField(max_length=100)
     release_date = models.DateField()
     popularity = models.FloatField()
     vote_average = models.FloatField()
     overview = models.TextField()
+    youtube_key = models.CharField(max_length=200)
     backdrop_path = models.CharField(max_length=200, null=True)
     poster_path = models.CharField(max_length=200, null=True)
-
+    def __str__(self):
+        return self.name
 class UpcomingMovie(models.Model):
     like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_upcoming_movies')
     genres = models.ManyToManyField(Genre, related_name='upcoming_movies')
+    actors = models.ManyToManyField(Actor, related_name='upcoming_actors')
+    director = models.ManyToManyField(Director, related_name='upcoming_director')
     adult = models.BooleanField()
-    movie_id = models.IntegerField(null=True)
     title = models.CharField(max_length=100)
     release_date = models.DateField()
     popularity = models.FloatField()
     vote_average = models.FloatField()
     overview = models.TextField()
+    youtube_key = models.CharField(max_length=200)
     backdrop_path = models.CharField(max_length=200, null=True)
     poster_path = models.CharField(max_length=200, null=True)
-
+    def __str__(self):
+        return self.name
 class Review(models.Model):
     like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_reviews')
     title = models.CharField(max_length=100)
--- final-pjt-back/movies/views.py
+++ final-pjt-back/movies/views.py
@@ -1,11 +1,135 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 import requests
-# from .models import Movie
-# Create your views here.
+from .models import Director, Genre, PopularMovie, NowPlayingMovie, UpcomingMovie, Actor
+
+API_KEY = '1084b2e96727cbe4bd9c2a0e2fd99168'
+GENRE_URL = 'https://api.themoviedb.org/3/genre/movie/list'
+POPULAR_MOVIE_URL = 'https://api.themoviedb.org/3/movie/popular'
+
+def genre_data():
+    response = requests.get(
+        GENRE_URL,
+        params={
+            'api_key': API_KEY,
+            'language': 'ko-kr',            
+        }
+    ).json()
+
+    for genre in response.get('genres'):
+        Genre.objects.create(
+            id=genre.get('id'),
+            name=genre.get('name')
+        )
+    return HttpResponse(response)
+
+# def get_youtube_key(movie_dict):    
+#     movie_id = movie_dict.get('id')
+#     response = requests.get(
+#         f'https://api.themoviedb.org/3/movie/{movie_id}/videos',
+#         params={
+#             'api_key': API_KEY
+#         }
+#     ).json()
+#     for video in response.get('results'):
+#         if video.get('site') == 'YouTube':
+#             return video.get('key')
+#     return 'nothing'
+
+# # movie에 출연한 배우 정보
+# def get_actors(movie):
+#     movie_id = movie.id
+#     response = requests.get(
+#         f'https://api.themoviedb.org/3/movie/{movie_id}/credits',
+#         params={
+#             'api_key': API_KEY,
+#             'language': 'ko-kr',
+#         }
+#     ).json()
+    
+#     for person in response.get('cast'):
+#         if person.get('known_for_department') != 'Acting': continue
+#         actor_id = person.get('id')
+#         if not Actor.objects.filter(pk=actor_id).exists():
+#             Actor.objects.create(
+#                 id=person.get('id'),
+#                 name=person.get('name'),
+#                 profile_path=person.get('profile_path'),
+#                 character=person.get('character')
+#             )
+#         movie.actors.add(actor_id)
+#         if movie.actors.count() == 5:
+#             break
+
+# def get_directors(movie):
+#     movie_id = movie.id
+#     response = requests.get(
+#         f'https://api.themoviedb.org/3/movie/{movie_id}/credits',
+#         params={
+#             'api_key': API_KEY,
+#             'language': 'ko-kr',
+#         }
+#     ).json()
+    
+#     for person in response.get('crew'):
+#         if person.get('job') != 'Director': continue
+#         director_id = person.get('id')
+#         if not Director.objects.filter(pk=director_id).exists():
+#             Director.objects.create(
+#                 id=person.get('id'),
+#                 name=person.get('name')
+#             )
+#         movie.dirctor.add(director_id)
+#         if movie.dirctor.count() == 1:
+#             break
+
+# def movie_data(page=1):
+#     response = requests.get(
+#         POPULAR_MOVIE_URL,
+#         params={
+#             'api_key': API_KEY,
+#             'language': 'ko-kr',     
+#             'page': page,       
+#         }
+#     ).json()
+
+#     for movie_dict in response.get('results'):
+#         if not movie_dict.get('release_date'): continue   # 없는 필드 skip
+#         # 유투브 key 조회
+#         youtube_key = get_youtube_key(movie_dict)
+        
+#         movie = PopularMovie.objects.create(
+#             id=movie_dict.get('id'),
+#             adult=movie_dict.get('adult'),
+#             title=movie_dict.get('title'),
+#             release_date=movie_dict.get('release_date'),
+#             popularity=movie_dict.get('popularity'),
+#             vote_average=movie_dict.get('vote_average'),
+#             overview=movie_dict.get('overview'),
+#             backdrop_path=movie_dict.get('backdrop_path'),   
+#             poster_path=movie_dict.get('poster_path'),   
+#             youtube_key=youtube_key         
+#         )
+#         for genre_id in movie_dict.get('genre_ids', []):
+#             movie.genres.add(genre_id)
+
+#         # 배우들 저장
+#         get_actors(movie)
+#         # get_directors(movie)
+#         print('>>>', movie.title, '==>', movie.youtube_key)
+
+# # from .models import Movie
+# # Create your views here.
 
 def index(request):
+        # Genre.objects.all().delete
+        # Actor.objects.all().delete
+        # PopularMovie.objects.all().delete
 
-
+        genre_data()
+        # for i in range(1,51):
+        #         movie_data(i)
+        return HttpResponse('OK')
     # data = Movie()
 
     # movies = []
@@ -38,4 +162,3 @@ def index(request):
 
             #     "movies": movies 
             # }
-    return render(request, 'movies/index.html')
