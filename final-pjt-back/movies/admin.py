04a17a2733a84e781b5694fda34f3cca70ff2a5c
aeri <dofl8787@naver.com>
2022-05-24 11:24:02 +0900

    admin 활성화

--- final-pjt-back/movies/admin.py
+++ final-pjt-back/movies/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from .models import PopularMovie
 
 # Register your models here.
-admin.site.register(PopularMovie)
+class MovieAdmin(admin.ModelAdmin):
+    filter_horizontal = ( 'genres', 'actors', 'director')
+    exclude = ('like_users',)
+admin.site.register(PopularMovie,MovieAdmin)
